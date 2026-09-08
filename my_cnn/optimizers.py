83023fcfb7ff66686db3672efa25d544fc63cd6c
bartek03m <bartosz.majewski03szkola@gmail.com>
2026-09-08 18:47:57 +0200

    Add sgd_momentum

--- my_cnn/optimizers.py
+++ my_cnn/optimizers.py
@@ -20,11 +20,42 @@ class SGD:
                 layer.biases -= self.learning_rate * layer.bias_gradient
     
 
+class Momentum:
+    """SGD with Momentum optimizer"""
+    def __init__(self, beta=0.9):
+        self.layers = None
+        self.learning_rate = None
+        self.beta = beta
+
+    def lazy_load(self, layers, learning_rate):
+        self.layers = layers
+        self.learning_rate = learning_rate
+
+    def update(self):
+        """Update weights and biases with momentum"""
+        for layer in self.layers:
+            if hasattr(layer, 'weight_gradient') and layer.weight_gradient is not None:
+                if not hasattr(layer, 'v_w') or layer.v_w is None:
+                    layer.v_w = np.zeros_like(layer.weights)
+                    layer.v_b = np.zeros_like(layer.biases)
+
+                # v = beta * v + grad
+                layer.v_w = self.beta * layer.v_w + layer.weight_gradient
+                layer.v_b = self.beta * layer.v_b + layer.bias_gradient
+
+                # w = w - lr * v
+                layer.weights -= self.learning_rate * layer.v_w
+                layer.biases -= self.learning_rate * layer.v_b
+
+
 def get_optimizer(name):
     """Return an optimizer by name"""
     if name is None:
         return SGD()
-    elif name.lower() == 'sgd':
+    name_str = str(name).lower()
+    if name_str == 'sgd':
         return SGD()
+    elif name_str in ('momentum', 'sgd_momentum'):
+        return Momentum()
     else:
         return SGD() 
